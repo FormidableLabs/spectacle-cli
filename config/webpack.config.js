c2fe9e0365165f055e58f907a09f7b21a8c24d2f
Ryan Roemer <ryan.roemer@formidable.com>
2019-10-30 22:42:24 -0700

    WIP/ARCHIVE: Just move over all deleted files from spectacle branch.

---
+++ config/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+/**
+ * Base configuration for the CLI, core, and examples.
+ */
+
+module.exports = {
+  mode: 'development', // TODO: Adjust for production.
+  entry: null,  // TODO: Leave blank?
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    pathinfo: true, // TODO: REMOVE
+    filename: 'spectacle.min.js' // TODO: Do a different name???
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader']
+      },
+      // TODO: MDX support.
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      // TODO: Figure out user theme thing.
+      // 'spectacle-user-theme': "TODO"
+    }
+  }
+};
