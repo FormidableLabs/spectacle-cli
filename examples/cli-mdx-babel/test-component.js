162250afef21b1823202e9c53ba11dfb1e7b805f
Ryan Roemer <ryan.roemer@formidable.com>
2019-11-01 12:40:54 -0700

    Update mdx example to be clearer.

--- examples/cli-mdx-babel/test-component.js
+++ examples/cli-mdx-babel/test-component.js
@@ -2,7 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Test = ({ height }) => {
-  return <div style={{ height, width: '100%', backgroundColor: 'yellow' }} />;
+  return (
+    <div
+      style={{
+        height,
+        width: '100%',
+        backgroundColor: 'yellow',
+        fontSize: '2em',
+        color: 'blue'
+      }}
+    >
+      JSX React component
+    </div>
+  );
 };
 
 Test.propTypes = {
