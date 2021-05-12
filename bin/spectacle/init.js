6d2e90fa6fe531901caac9b1d82f2755b718b52b
Mone't Fulgham <monetfulgham@gmail.com>
2021-05-12 13:09:12 -0400

    Fix regex validation

--- bin/spectacle/init.js
+++ bin/spectacle/init.js
@@ -3,6 +3,11 @@
 'use strict';
 
 const prompts = require('prompts');
+
+const mdRegex = /(\.mdx$|\.md$)/gm;
+const jsRegex = /(\.json$|\.js$)/gm;
+const jsxRegex = /(\.jsx$|\.js$)/gm;
+
 const questions = [
   {
     type: 'select',
@@ -14,21 +19,23 @@ const questions = [
     ]
   },
   {
-    // TODO Only ask this question if 1 was the server option
     type: prev => (prev === 'server' ? 'number' : null),
     name: 'Port',
     message: 'What port for the live server?',
-    validate: val =>
-      val.toString().length === '4' ? true : 'The port must be 4 numbers'
+    validate: val => {
+      const numLength = val.toString().length;
+      if (numLength === 4) {
+        return val;
+      }
+      return 'The port should be 4 numbers';
+    }
   },
   {
     type: 'text',
     name: 'Filename of markdown source',
     message: 'What is the filename of the markdown source?',
     validate: name =>
-      name.includes('.md' || '.mdx')
-        ? true
-        : 'The file must be a mdx or md file'
+      name.match(mdRegex) ? true : 'The file must be a mdx or md file'
   },
   {
     type: 'confirm',
@@ -38,14 +45,12 @@ const questions = [
   },
   {
     // eslint-disable-next-line no-constant-condition
-    type: prev => (prev === 'yes' ? 'text' : null),
+    type: prev => (prev === 'y' || 'yes' ? 'text' : null),
     name: 'Custom Theme File name',
     message: 'What is the filename of the custom theme file?',
     // TODO : Only ask this question if 3 got a yes
     validate: name =>
-      name.includes('.js' || '.json')
-        ? true
-        : 'The file must be a js or json file'
+      name.match(jsRegex) ? true : 'The file must be a js or json file'
   },
   {
     type: 'confirm',
@@ -54,13 +59,12 @@ const questions = [
     initial: false
   },
   {
-    type: prev => (prev === 'yes' ? 'text' : null),
+    // eslint-disable-next-line no-constant-condition
+    type: prev => (prev === 'y' || 'yes' ? 'text' : null),
     name: 'Custom Template File name',
     message: 'What is the filename of the custom template file?',
     validate: name =>
-      name.includes('.js' || '.jsx')
-        ? true
-        : 'The file must be a jsx or js file'
+      name.match(jsxRegex) ? true : 'The file must be a jsx or js file'
   }
 ];
 
